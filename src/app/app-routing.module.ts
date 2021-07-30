df822035ae4b1bc11ac0ccc06a9c4f9d2db48ed4
Eugene <yoliinyk@brightgrove.com>
2021-07-30 18:32:34 +0300

    wallet route added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SignInComponent} from "./account/sign-in/sign-in.component";
 import {SignUpComponent} from "./account/sign-up/sign-up.component";
+import {CategoryCardComponent} from "./category-card/category-card.component";
 
 const routes: Routes = [
+  {path: 'wallet', component: CategoryCardComponent},
   {path: 'sign-in', component: SignInComponent, data: {animation: 'isRight'}},
   {path: 'sign-up', component: SignUpComponent, data: {animation: 'isLeft'}},
   {path: '**', component: SignUpComponent}
