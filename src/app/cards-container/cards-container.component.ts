9e1b01e6fdeb4dba5fef718999c4821a68aa3fa8
Eugene <yoliinyk@brightgrove.com>
2021-08-04 16:54:09 +0300

    Change let to const

--- src/app/cards-container/cards-container.component.ts
+++ src/app/cards-container/cards-container.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import Transaction from "../entities/transaction.entity";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import UserService from "../services/user.service";
 
 @Component({
   selector: 'cards-container',
@@ -10,74 +13,26 @@ export class CardsContainerComponent implements OnInit  {
 
   category_transactions = new Map<string, Transaction[]>()
 
-  constructor() { }
+  constructor(private http: HttpClient,
+              private userService: UserService) { }
 
   ngOnInit(): void {
     this.category_transactions = new Map<string, Transaction[]>()
-    // mock data
-    this.category_transactions.set("Category 1", [
-      {
-        amount: 1020,
-        timestamp: "11-12-2021"
-      },
-      {
-        amount: 1020,
-        timestamp: "11-12-2021"
-      },
-    ])
-    this.category_transactions.set("Category 2", [
-      {
-        amount: 2020,
-        timestamp: "12-12-2021"
-      },
-      {
-        amount: 2020,
-        timestamp: "12-12-2021"
-      },
-      {
-        amount: 13020,
-        timestamp: "19-12-2021"
-      }
-    ])
-    this.category_transactions.set("Category 3", [
-      {
-        amount: 302.50,
-        timestamp: "11-11-2021"
-      },
-      {
-        amount: 2020,
-        timestamp: "11-10-2021"
-      },
-    ])
-    this.category_transactions.set("Category 4", [
-      {
-        amount: 1020,
-        timestamp: "11-12-2021"
-      },
-      {
-        amount: 1020,
-        timestamp: "11-12-2021"
-      },
-    ])
-    this.category_transactions.set("Category 5", [
-      {
-        amount: 102,
-        timestamp: "11-12-2021"
-      },
-      {
-        amount: 1020,
-        timestamp: "11-12-2021"
-      },
-      {
-        amount: 10203.75,
-        timestamp: "8-10-2021"
-      }
-    ])
+    this.http.get<Transaction[]>(environment.serverUrl + `/users/${this.userService.getCurrentUser().id}/summary`)
+      .subscribe(transactions => {
+        console.log(transactions)
+        for (const transaction of transactions) {
+          const containedTransactions = this.category_transactions.get(transaction.categoryName!)
+          const newSet: Transaction[] = containedTransactions == null ? [transaction] : [...containedTransactions, transaction]
+
+          this.category_transactions.set(transaction.categoryName!, newSet)
+        }
+      })
   }
 
   calculateAmountForMonth(transactions: Transaction[]): number {
     let amountForMonth = 0
-    for (let transaction of transactions)
+    for (const transaction of transactions)
       amountForMonth += transaction.amount
 
     return amountForMonth
