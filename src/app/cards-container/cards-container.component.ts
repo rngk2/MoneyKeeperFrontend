9ef26039ae927223abea7e9db3acab05794d88eb
Eugene <yoliinyk@brightgrove.com>
2021-08-06 15:12:30 +0300

    Filter transactions with amount=0.

--- src/app/cards-container/cards-container.component.ts
+++ src/app/cards-container/cards-container.component.ts
@@ -28,12 +28,18 @@ export class CardsContainerComponent implements OnInit {
     this.http.get<Transaction[]>(environment.serverUrl + `/users/10/summary`)
       .subscribe(transactions => {
         this.category_transactions = new Map<string, Transaction[]>()
+
         for (const transaction of transactions) {
-          const containedTransactions = this.category_transactions.get(transaction.categoryName!)
-          const newSet: Transaction[] = containedTransactions == null ? [transaction] : [...containedTransactions, transaction]
-          this.category_transactions.set(transaction.categoryName!, newSet)
-        }
+            const containedTransactions = this.category_transactions.get(transaction.categoryName!)
+            const newSet: Transaction[] = containedTransactions == null ? this.getEmptyArrayIfTransactionIsSpoiled(transaction)
+              : [...containedTransactions, ...this.getEmptyArrayIfTransactionIsSpoiled(transaction)]
+            this.category_transactions.set(transaction.categoryName!, newSet)
+         }
       })
+  }
+
+  private getEmptyArrayIfTransactionIsSpoiled(transaction: Transaction): Transaction[] {
+    return transaction.amount === 0 ? [] : [transaction]
   }
 
   calculateAmountForMonth(transactions: Transaction[]): number {
