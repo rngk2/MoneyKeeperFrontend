543b64f78946414d4c2ca7934fc160bda2d6452d
Eugene <yoliinyk@brightgrove.com>
2021-08-09 14:53:21 +0300

    Remove unused imports.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SignInComponent} from "./account/sign-in/sign-in.component";
 import {SignUpComponent} from "./account/sign-up/sign-up.component";
-import {CategoryCardComponent} from "./category-card/category-card.component";
-import {CardsContainerComponent} from "./cards-container/cards-container.component";
 import {WalletPageComponent} from "./wallet-page/wallet-page.component";
-import {AddTransactionFormComponent} from "./add-transaction-form/add-transaction-form.component";
 import {Page404Component} from "./page404/page404.component";
 
 const routes: Routes = [
