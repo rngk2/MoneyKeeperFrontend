d9dad958ca27af1f02910426c885f09aedfab82c
Eugene <yoliinyk@brightgrove.com>
2021-08-13 18:23:30 +0300

    Add search across transactions.

--- src/app/all-transactions-page/all-transactions-page.component.ts
+++ src/app/all-transactions-page/all-transactions-page.component.ts
@@ -4,6 +4,22 @@ import {HttpClient} from "@angular/common/http";
 import UserService from "../services/user.service";
 import {Range} from "src/app/utils/Utils"
 import {BASE_SERVER_URL} from "../app.config";
+import {FormControl} from "@angular/forms";
+import {debounceTime, distinctUntilChanged} from "rxjs/operators";
+
+class RangeOffsetController {
+
+  constructor(private beginOffset: number = 0,
+              private step: number = 20) { }
+
+  getNextRange(): Range {
+    const begin = this.beginOffset
+    const end = begin + this.step
+    this.beginOffset += this.step + 1
+
+    return { begin, end }
+  }
+}
 
 @Component({
   selector: 'all-transactions-list',
@@ -13,16 +29,24 @@ import {BASE_SERVER_URL} from "../app.config";
 export class AllTransactionsPageComponent implements OnInit {
 
   public transactions: Set<Transaction> = new Set<Transaction>()
+  public searchControl = new FormControl('')
 
-  private step = 20
-  private beginOffset = 0
+  private rangeForAll = new RangeOffsetController()
+  private rangeForSearch = new RangeOffsetController()
+  private static readonly SEARCH_DEBOUNCE_DURATION = 400
 
   constructor(private readonly http: HttpClient,
               private readonly userService: UserService,
               @Inject(BASE_SERVER_URL) private readonly serverUrl: string) { }
 
   public ngOnInit(): void {
-    this.fetchTransactions(this.getNextRange())
+    this.searchControl.valueChanges
+      .pipe(debounceTime(AllTransactionsPageComponent.SEARCH_DEBOUNCE_DURATION), distinctUntilChanged())
+      .subscribe(() => {
+        [this.rangeForSearch, this.rangeForSearch] = [new RangeOffsetController(), new RangeOffsetController()]
+        this.fetchTransactionsWithPattern(this.rangeForSearch.getNextRange())
+      })
+    this.fetchTransactions(this.rangeForAll.getNextRange())
   }
 
   private fetchTransactions(range: Range): void {
@@ -30,19 +54,22 @@ export class AllTransactionsPageComponent implements OnInit {
       .subscribe(transactions => this.transactions =  new Set<Transaction>(this.sortByDate([...this.transactions, ...transactions])))
   }
 
-  private sortByDate(transactions: Transaction[]): Transaction[] {
-    return transactions.sort((a, b) => <any>new Date(b.timestamp) - <any>new Date(a.timestamp))
+  private fetchTransactionsWithPattern(range: Range): void {
+    const pattern = `${this.searchControl.value}%`
+    this.http.get<Transaction[]>(this.serverUrl + `/transactions/${this.userService.getCurrentUser().id}/${range.begin}/${range.end}/${pattern}`)
+      .subscribe(transactions => {
+        const append = range.begin === 0 ? new Set<Transaction>([]) : this.transactions
+        this.transactions = new Set<Transaction>(this.sortByDate([...append, ...transactions]))
+      })
   }
 
-  private getNextRange(): Range {
-    const begin = this.beginOffset
-    const end = begin + this.step
-    this.beginOffset += this.step + 1
-
-    return { begin, end }
+  private sortByDate(transactions: Transaction[]): Transaction[] {
+    return transactions.sort((a, b) => <number><unknown>new Date(b.timestamp) - <number><unknown>new Date(a.timestamp))
   }
 
   public onScroll(): void {
-    this.fetchTransactions(this.getNextRange())
+    this.searchControl!.value.length === 0 ?
+      this.fetchTransactions(this.rangeForAll.getNextRange()) :
+      this.fetchTransactionsWithPattern(this.rangeForSearch.getNextRange())
   }
 }
