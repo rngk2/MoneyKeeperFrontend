d41a83e2016f816904d5bc8b912816ffd0e21985
Eugene <yoliinyk@brightgrove.com>
2021-08-31 10:30:32 +0300

    Refactor convertToObserved func.

--- src/app/utils/Utils.ts
+++ src/app/utils/Utils.ts
@@ -13,12 +13,13 @@ export type Observed<T> = {
 }
 
 export const convertToObserved = <T>(value: T): Observed<T> => {
-  let result : any = {};
+  let result: any = {};
 
   for (const key in value) {
-    const func = value[key] as unknown as (...args: any[]) => any;
-    if (func instanceof Function) {
-      result[key] = (...args: any[]) => fromPromise(func(...args))
+    const member = value[key];
+    if (member instanceof Function) {
+      let f: Function = member as unknown as (...args: any[]) => any;
+      result[key] = (...args: any[]) => fromPromise(f(...args))
     }
   }
 
