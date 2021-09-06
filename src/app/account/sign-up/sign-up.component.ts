becf25e9681073e5e7ec321952f3cb6e1f7d0c0b
Eugene <yoliinyk@brightgrove.com>
2021-09-06 16:39:21 +0300

    Use Validators.email instead of pattern.

--- src/app/account/sign-up/sign-up.component.ts
+++ src/app/account/sign-up/sign-up.component.ts
@@ -31,7 +31,7 @@ export class SignUpComponent implements OnDestroy {
       ]),
       email: new FormControl('', [
         Validators.required,
-        Validators.pattern(/^(([^<>()\[\]\\.,;:\s@']+(\.[^<>()\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+        Validators.email
       ]),
       password: new FormControl('', [
         Validators.required,
