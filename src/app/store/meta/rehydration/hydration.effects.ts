33a0b0c38a1945f16843b50efd64a95b14090f02
Eugene <yoliinyk@brightgrove.com>
2021-09-20 17:00:01 +0300

    Fix rehydration.

--- src/app/store/meta/rehydration/hydration.effects.ts
+++ src/app/store/meta/rehydration/hydration.effects.ts
@@ -1,44 +1,33 @@
 import {Injectable} from "@angular/core";
 import {Actions, createEffect, ofType, OnInitEffects} from "@ngrx/effects";
-import {Action, Store} from "@ngrx/store";
-import AppState from "../../app.state";
-import {distinctUntilChanged, map, switchMap, tap} from "rxjs/operators";
+import {Action} from "@ngrx/store";
+import {map} from "rxjs/operators";
 import {HydrationActions} from "./hydration.actions";
+import {LOCALSTORAGE_STATE_PATH} from "../../../constants";
 
 @Injectable()
 export class HydrationEffects implements OnInitEffects {
 
-  constructor(private readonly actions$: Actions,
-              private readonly store: Store<AppState>,) { }
+  constructor(private readonly actions$: Actions) { }
 
   public readonly hydrate = createEffect(() =>
     this.actions$.pipe(
       ofType(HydrationActions.Hydrate),
       map(() => {
-        const storageValue = localStorage.getItem('state');
+        const storageValue = localStorage.getItem(LOCALSTORAGE_STATE_PATH);
         if (storageValue) {
           try {
             const state = JSON.parse(storageValue);
             return HydrationActions.HydrationSuccess({state});
           }
           catch {
-            localStorage.removeItem('state');
+            localStorage.removeItem(LOCALSTORAGE_STATE_PATH);
           }
         }
         return HydrationActions.HydrationFailure();
       })
     )
   )
-
-  public readonly serialize = createEffect(() =>
-      this.actions$.pipe(
-        ofType(HydrationActions.HydrationSuccess),
-        switchMap(() => this.store),
-        distinctUntilChanged(),
-        tap((state) => localStorage.setItem("state", JSON.stringify(state)))
-      ),
-    { dispatch: false }
-  );
 
   ngrxOnInitEffects(): Action {
     return HydrationActions.Hydrate();
