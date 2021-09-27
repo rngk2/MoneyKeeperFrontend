c8448f46083e02d3f42086ca97239ea5107aad6a
Eugene <yoliinyk@brightgrove.com>
2021-09-27 17:53:59 +0300

    Remove useless enum.

--- src/app/store/user/auth.actions.ts
+++ src/app/store/user/auth.actions.ts
@@ -4,22 +4,32 @@ import { CreateUserDto, IError } from "../../../api/api.generated";
 import IUser from "../../entities/user.entity";
 
 export namespace AuthActions {
-  enum AuthActionTypes {
-    LOGIN = '[Auth] Login',
-    LOGIN_SUCCESS = '[Auth] Login Success',
-    LOGIN_FAILURE = '[Auth] Login Failure',
-    LOGOUT = '[Auth] Logout',
-    SIGNUP = '[Auth] Signup',
-    SIGNUP_SUCCESS = '[Auth] Signup Success',
-    SIGNUP_FAILURE = '[Auth] Signup Failure'
-  }
-
-  export const LogIn = createAction(AuthActionTypes.LOGIN, props<{ email: string, password: string }>());
-  export const LogInSuccess = createAction(AuthActionTypes.LOGIN_SUCCESS, props<IUser>());
-  export const LogInFailure = createAction(AuthActionTypes.LOGIN_FAILURE, props<IError>());
-  export const LogOut = createAction(AuthActionTypes.LOGOUT);
-  export const SignUp = createAction(AuthActionTypes.SIGNUP, props<CreateUserDto>());
-  export const SignUpSuccess = createAction(AuthActionTypes.SIGNUP_SUCCESS, props<IUser>());
-  export const SignUpFailure = createAction(AuthActionTypes.SIGNUP_FAILURE, props<IError>());
+  export const LogIn = createAction(
+    '[Auth] Login',
+    props<{ email: string, password: string }>()
+  );
+  export const LogInSuccess = createAction(
+    '[Auth] Login Success',
+    props<IUser>()
+  );
+  export const LogInFailure = createAction(
+    '[Auth] Login Failure',
+    props<IError>()
+  );
+  export const LogOut = createAction(
+    '[Auth] Logout'
+  );
+  export const SignUp = createAction(
+    '[Auth] Signup',
+    props<CreateUserDto>()
+  );
+  export const SignUpSuccess = createAction(
+    '[Auth] Signup Success',
+    props<IUser>()
+  );
+  export const SignUpFailure = createAction(
+    '[Auth] Signup Failure',
+    props<IError>()
+  );
 }
 
